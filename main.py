0787b0f3ce9a84a484ac67e82ce9ae981408e4d4
Adam Strawmyer <adam@strawmyer.com>
2020-04-19 11:34:50 -0500

    function for usa or OK

--- main.py
+++ main.py
@@ -5,15 +5,35 @@ import json
 import matplotlib.pyplot as plt
 import numpy as np
 
-content = requests.get("https://covidtracking.com/api/us/daily")
-json = json.loads(content.content)
-pos = []
-for x in json:
-    pos.insert(0,x['positive'])
-    #print(x['positive'])
+usaData = requests.get("https://covidtracking.com/api/us/daily")
+jsonUSA = json.loads(usaData.content)
 
-print(pos)
-xaxis = list(range(len(pos)))
+stateData = requests.get("https://covidtracking.com/api/v1/states/daily.json")
+jsonStates = json.loads(stateData.content)
 
-plt.plot(pos)
-plt.show()
+def USApos():
+    pos = []
+    for x in jsonUSA:
+        pos.insert(0,x['positive'])
+    plt.plot(pos)
+    plt.show()
+
+def StatePos():
+    pos = []
+    for x in jsonStates:
+        if x['state'] == "OK":
+            pos.insert(0,x['positive'])
+    plt.plot(pos)
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main_switch_function = {"1": USApos, "2": StatePos, "3": USApos, "4": USApos, "5": exit}
+    while True:
+        print("What do you want to do?")
+        response = input("1. USA Positive chart, 2. Create a Report, 3. Send all letters, 4. Projection, 5. Quit: ")
+        try:
+            main_switch_function.get(response)()
+        except TypeError:
+            print("Not a valid input.")
