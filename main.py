87a395187ff6736e9624023fe5727d95af5b05f9
Adam Strawmyer <adam@strawmyer.com>
2020-04-18 20:21:16 -0500

    plot US data

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+""" Reads US daily data from covidtracking.com and makes a graph of positive per day. """
+
 import requests
 import json
+import matplotlib.pyplot as plt
+import numpy as np
+
 content = requests.get("https://covidtracking.com/api/us/daily")
 json = json.loads(content.content)
-
+pos = []
 for x in json:
-    print(x['positive']).
+    pos.insert(0,x['positive'])
+    #print(x['positive'])
+
+print(pos)
+xaxis = list(range(len(pos)))
+
+plt.plot(pos)
+plt.show()
